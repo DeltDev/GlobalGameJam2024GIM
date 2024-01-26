4dc2b561f628376234d84b778617c0f0385deabf
Addin Munawwar <moonawar@protonmail.com>
2024-01-27 03:06:54 +0700

    feat: visual integration and enemy: birjon

---
+++ Assets/Scripts/Enemies/Birjon.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Birjon : MonoBehaviour
+{
+    public Transform player;
+    [Header("Birjon Settings")]
+    public float distanceToStartAttack = 1f;
+    public float attackDelay = 1.2f;
+    public float postAttackDelay = 0.8f;
+    [Header("Projectile Settings")]
+    public GameObject projectilePrefab;
+    public int numOfProjectiles = 5;
+    public float projSpeed = 0.5f;
+    public float delayBetweenProjectiles = 0.2f;
+    public float amplitude = 1f;
+    public float frequency = 10f;
+    private bool isAttacking = false;
+
+    private IEnumerator LaunchProjectiles() {
+        yield return new WaitForSeconds(attackDelay);
+
+        Vector2 direction = (player.position - transform.position).normalized;
+        for (int i = 0; i < numOfProjectiles; i++) {
+            GameObject projectile = Instantiate(projectilePrefab, transform.position, Quaternion.identity);
+            projectile.GetComponent<BirjonProjectile>().Launch(
+                direction, projSpeed, amplitude: amplitude, frequency: frequency
+            );
+            yield return new WaitForSeconds(delayBetweenProjectiles);
+        }   
+        yield return new WaitForSeconds(postAttackDelay);
+    }
+    
+    private IEnumerator StartAttack() {
+        isAttacking = true;
+        yield return StartCoroutine(LaunchProjectiles());
+        isAttacking = false;
+    }
+
+    void Update()
+    {
+        if (isAttacking) return;
+        
+        Vector2 distanceVector = player.position - transform.position;
+        transform.position += (Vector3) distanceVector.normalized * Time.deltaTime;
+
+        transform.rotation = Quaternion.Euler(0, distanceVector.x < 0 ? 0 : 180, 0);
+
+        if (playerIsInAttackRange()) {
+            StartCoroutine(StartAttack());
+        }
+    }
+
+
+    private bool playerIsInAttackRange() {
+        return Vector2.Distance(transform.position, player.position) < distanceToStartAttack;
+    }
+}
---
+++ Assets/Scripts/Enemies/BirjonProjectile.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirjonProjectile : MonoBehaviour
+{
+
+    public bool isLaunched = false; 
+    private Vector2 direction;
+    private Vector2 perpendicularDirection;
+    private float projSpeed;
+    private float amplitude;
+    private float frequency;
+    private float startTime;
+
+    public void Launch(Vector2 direction, float projSpeed = 0.5f, float amplitude = 0.5f, float frequency = 1f) {
+        this.direction = direction.normalized;
+        this.perpendicularDirection = new Vector2(-direction.y, direction.x);
+        this.projSpeed = projSpeed;
+        this.amplitude = amplitude;
+        this.frequency = frequency;
+        this.startTime = Time.time;
+        isLaunched = true;
+    }
+
+    void Update()
+    {
+        if (!isLaunched) return;
+        transform.position += (Vector3) direction * Time.deltaTime * projSpeed;
+
+        float timeElapsed = Time.time - startTime;
+        float yOffSet = Mathf.Sin(timeElapsed * frequency) * amplitude;
+        transform.position += (Vector3) perpendicularDirection * yOffSet * Time.deltaTime;        
+    }
+}

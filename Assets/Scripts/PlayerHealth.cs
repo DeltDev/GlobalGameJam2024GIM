d371e47c82b308513b31fbe9c7b0ac9aa4328d38
DeltDev <deltenc88@gmail.com>
2024-01-28 06:38:33 +0700

    more sfx

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -57,6 +57,7 @@ public class PlayerHealth : MonoBehaviour
         }
         if (collision.tag == "ExtraHP")
         {
+            audioManager.PlaySound("PotionSFX");
             TakeDamage(-HealthGain);
             Destroy(collision.gameObject);
             if (CurrentHealth > MaxHealth)
--- Assets/Scripts/UI/ObjectiveBox.cs
+++ Assets/Scripts/UI/ObjectiveBox.cs
@@ -14,8 +14,10 @@ public class ObjectiveBox : MonoBehaviour
 
 
     private RectTransform rectTransform;
+    private AudioManager audioManager;
     private void Awake() {
         rectTransform = GetComponent<RectTransform>();
+        audioManager = GameObject.Find("AudioManager").GetComponent<AudioManager>();
     }
 
     private void Start() {
@@ -25,8 +27,10 @@ public class ObjectiveBox : MonoBehaviour
 
     private IEnumerator ObjectiveBoxCor() {
         yield return new WaitForSeconds(delayBeforeAppearance);
+        audioManager.PlaySound("ObjectiveOn");
         rectTransform.DOLocalMove(appearPosition, open_closeDuration).SetEase(Ease.OutBack);
         yield return new WaitForSeconds(open_closeDuration + stayDuration);
+        audioManager.PlaySound("ObjectiveOff");
         rectTransform.DOLocalMove(disappearPosition, open_closeDuration).SetEase(Ease.InBack);
     }
 

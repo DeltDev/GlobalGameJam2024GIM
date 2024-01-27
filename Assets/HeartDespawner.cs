60acd9543793a6f5eff9473333e1cfbe18e5530f
DeltDev <deltenc88@gmail.com>
2024-01-27 20:44:05 +0700

    Despawn Heart Script

---
+++ Assets/HeartDespawner.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeartDespawner : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private float DespawnTime;
+    void Start()
+    {
+        DespawnTime = 5f;
+        StartCoroutine(DespawnHeart(DespawnTime));
+    }
+
+    // Update is called once per frame
+    IEnumerator DespawnHeart(float DespawnTime)
+    {
+        yield return new WaitForSeconds(DespawnTime);
+        Destroy(this.gameObject);
+    }
+}
